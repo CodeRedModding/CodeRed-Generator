8b44d1d78ed029c448efa5fffed53a68acd47b54
BranK <23649425+ItsBranK@users.noreply.github.com>
2024-04-20 22:12:17 -0400

    Update Configuration.cpp

--- Engine/Template/Configuration.cpp
+++ Engine/Template/Configuration.cpp
@@ -83,7 +83,7 @@ uint32_t GConfig::m_gameAlignment = 0x4;
 uint32_t GConfig::m_finalAlignment = 0x4;
 
 // Names of classes or structs you don't want generated in the final sdk.
-std::vector<std::string> GConfig::m_blacklistedTypes = { "FPointer", "FQWord", "FScriptDelegate" };
+std::vector<std::string> GConfig::m_blacklistedTypes = { "Pointer", "QWord", "ScriptDelegate" };
 
 // Names of classes or structs you want to override with your own custom one.
 std::map<std::string, std::string> GConfig::m_typeOverrides = {
